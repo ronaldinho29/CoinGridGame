0b3cdd3d79e9c7d1f50a99a044963a21aa3d4c60
Jared Obispo <jsobispo@terpmail.umd.edu>
2024-03-28 03:17:38 -0400

    Added user wallet and a flame for choosing incorrectly

--- nextbigthing/lib/main.dart
+++ nextbigthing/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mongo_dart/mongo_dart.dart' as mongo;
-
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -101,7 +101,6 @@ class MainMenu extends StatelessWidget {
   }
 }
 
-
 class StartGame extends StatefulWidget {
   final String profileName;
 
@@ -112,9 +111,15 @@ class StartGame extends StatefulWidget {
 }
 
 class _StartGameState extends State<StartGame> {
-  List<bool> _isTitleVisible = List.generate(30, (index) => true);
+  final int _numberOfTiles = 30;
+  late int _tileWithRedX;
+  List<bool> _isTileClicked;
   List<bool> _isBorderVisible = List.generate(30, (index) => true);
-  List<bool> _isTileEnabled = List.generate(30, (index) => true);
+
+  _StartGameState() : _isTileClicked = List.generate(30, (index) => false) {
+    // Randomly assign one tile to contain a red X mark
+    _tileWithRedX = Random().nextInt(30);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -127,49 +132,76 @@ class _StartGameState extends State<StartGame> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body: GridView.count(
-        crossAxisCount: 5, // Changed to 5x6 grid
-        children: List.generate(30, (index) {
-          return GestureDetector(
-            onTap: () {
-              if (_isTileEnabled[index]) {
-                setState(() {
-                  if (_isTitleVisible[index]) {
-                    _isTitleVisible[index] = false;
-                  } else {
-                    _isBorderVisible[index] = !_isBorderVisible[index];
+      body: Column(
+        children: [
+          GridView.count(
+            crossAxisCount: 5,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            children: List.generate(_numberOfTiles, (index) {
+              return GestureDetector(
+                onTap: () {
+                  if (!_isTileClicked[index]) {
+                    setState(() {
+                      _isTileClicked[index] = true; // Mark tile as clicked
+                    });
                   }
-                  _isTileEnabled[index] = false; // Disable the tile
-                });
-              }
-            },
-            child: Stack(
-              children: [
-                Visibility(
-                  visible: _isBorderVisible[index],
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black), // Add black borders
-                    ),
-                  ),
-                ),
-                Visibility(
-                  visible: _isTitleVisible[index],
-                  child: Center(
-                    child: Container(
-                      width: 70, // Adjust the width of the green tile
-                      height: 70, // Adjust the height of the green tile
-                      decoration: BoxDecoration(
-                        color: Colors.green, // Green color tile
-                        borderRadius: BorderRadius.circular(5), // Rounded edges
+                },
+                child: Stack(
+                  children: [
+                    Visibility(
+                      visible: _isBorderVisible[index],
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                        ),
                       ),
                     ),
-                  ),
+                    Visibility(
+                      visible: !_isTileClicked[index],
+                      child: Center(
+                        child: Container(
+                          width: 70,
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // Conditionally show the red X mark if this tile is the one and has been clicked
+                    if (_isTileClicked[index] && index == _tileWithRedX)
+                      Center(
+                        child:
+                            Icon(Icons.whatshot, size: 70, color: Colors.red),
+                      ),
+                  ],
                 ),
-              ],
+              );
+            }),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: EdgeInsets.all(
+                  2.0), // Adds margin around the wallet container
+              padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('\$1000.00', style: TextStyle(fontSize: 20)),
+                  SizedBox(width: 10),
+                  Icon(Icons.add_card, color: Colors.blue),
+                ],
+              ),
             ),
-          );
-        }),
+          ),
+        ],
       ),
     );
   }
