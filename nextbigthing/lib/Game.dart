2eef04507de562cf162d933462bd8680f487e507
Jared Obispo <jsobispo@terpmail.umd.edu>
2024-04-12 18:53:21 -0400

    Added Redemption game

--- nextbigthing/lib/Game.dart
+++ nextbigthing/lib/Game.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:mongo_dart/mongo_dart.dart' as mongo;
@@ -13,7 +14,7 @@ class Game extends ChangeNotifier {
   bool gameOver = false;
   bool gameStarted = false;
   bool userWon = false;
-  bool redemptionWon = false; 
+  bool redemptionWon = false;
   String databaseUri =
       'mongodb+srv://ronaldchomnou:Ronaldinho2910@cluster0.39ac5k2.mongodb.net/nextbigthing?retryWrites=true&w=majority&appName=Cluster0';
 
@@ -138,31 +139,145 @@ class Game extends ChangeNotifier {
     }
   }
 
- void _showMineHitDialog() async {
-  await showDialog(
-    context: context,
-    barrierDismissible: false,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Center(child: Text("Mine Hit!")), // Center the title
-        content: Text("Do you want to play a redemption game?"),
-        actions: <Widget>[
-          TextButton(
-            child: Text("Yes"),
-            onPressed: () {
-              resetGame();
-              Navigator.of(context).pop();
-            },
-          ),
-          TextButton(
-            child: Text("No"),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
+  void _showMineHitDialog() async {
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(child: Text("Mine Hit!")),
+          content: Text("Do you want to play a redemption game?"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Yes"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _showQuestionDialog();
+              },
+            ),
+            TextButton(
+              child: Text("No"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  final List<Map<String, String>> questionsAndAnswers = [
+    {'question': 'What is 5 + 5?', 'answer': '10'},
+    {'question': 'What is the capital of France?', 'answer': 'Paris'},
+    {'question': 'What is 7 * 6?', 'answer': '42'},
+    {
+      'question': 'What is the color of the Statue of Liberty?',
+      'answer': 'Green'
     },
-  );
+    {'question': 'Who is the best CS professor at UMD?', 'answer': 'Marsh'},
+    {'question': 'How many tiles are there in this game?', 'answer': '30'},
+    {'question': 'What color is the sun?', 'answer': 'Yellow'},
+    {
+      'question': 'What year did the Covid-19 pandemic start?',
+      'answer': '2020'
+    },
+    {'question': 'What has keys but can\'t open locks?', 'answer': 'Piano'},
+    {'question': 'What has a ring but no fingers?', 'answer': 'Phone'},
+  ];
+  //Redemption Game
+  void _showQuestionDialog() async {
+    TextEditingController textEditingController = TextEditingController();
+
+    var random = Random();
+    var selectedQA =
+        questionsAndAnswers[random.nextInt(questionsAndAnswers.length)];
+
+    void closeDialog() {
+      if (Navigator.canPop(context)) {
+        Navigator.of(context).pop();
+      }
+    }
+
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(child: Text("Enter Your Answer")),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(selectedQA['question']!),
+              TextField(controller: textEditingController),
+              SizedBox(height: 20),
+              CountdownTimerDisplay(
+                  initialTime: 5, onTimerComplete: closeDialog),
+            ],
+          ),
+          actions: <Widget>[
+            Center(
+              child: TextButton(
+                child: Text("Submit"),
+                onPressed: () {
+                  if (textEditingController.text.trim().toLowerCase() ==
+                      selectedQA['answer']!.toLowerCase()) {
+                    closeDialog();
+                    resetGame();
+                  } else {
+                    closeDialog();
+                  }
+                },
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
+
+//Timer
+class CountdownTimerDisplay extends StatefulWidget {
+  final int initialTime;
+  final VoidCallback onTimerComplete;
+
+  CountdownTimerDisplay(
+      {Key? key, required this.initialTime, required this.onTimerComplete})
+      : super(key: key);
+
+  @override
+  _CountdownTimerDisplayState createState() => _CountdownTimerDisplayState();
+}
+
+class _CountdownTimerDisplayState extends State<CountdownTimerDisplay> {
+  late int remainingTime;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    remainingTime = widget.initialTime;
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (remainingTime > 0) {
+        setState(() {
+          remainingTime--;
+        });
+      } else {
+        timer.cancel();
+        widget.onTimerComplete();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Time left: $remainingTime seconds');
+  }
 }
